1a6a4a04a4ee41628e2746c9f006c6361e7a3e5e
Ryan Van Etten <ryanve@gmail.com>
2014-02-27 06:22:22 -0500

    Add tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,16 @@
 (function(root) {
   var common = typeof module != 'undefined' && !!module.exports
     , aok = common ? require('../node_modules/aok') : root.aok
-    , avant = root.avant;
-  aok.log(avant);
+    , avant = root.avant
+    , html = document.documentElement
+    , body = document.body;
+    
+  aok('methods', !aok.fail(['addEvent', 'removeEvent', 'hasEvent'], function(n) {
+    return typeof avant[n] == 'function';
+  }));
+
+  aok('hasEvent-1', true === avant.hasEvent('blur'));
+  aok('hasEvent-2', true === avant.hasEvent('blur', html));
+  aok('hasEvent-3', false === avant.hasEvent('!!!'));
+  aok('hasEvent-4', 2 === aok.pass(['blur', 'click'], avant.hasEvent, body));
 }(this));
