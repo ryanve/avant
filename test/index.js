a64e546e22e8cbc3113191c0bd29c178333483e3
Ryan Van Etten <ryanve@gmail.com>
2014-09-19 04:29:21 -0700

    Update tests

--- test/index.js
+++ test/index.js
@@ -3,16 +3,22 @@
     , aok = doc ? root.aok : require('aok')
     , avant = doc ? root.avant : require('../src')
     , html = doc.documentElement
-    , body = doc.body;
-    
-  aok('methods', !aok.fail(['addEvent', 'removeEvent', 'hasEvent'], function(n) {
+    , body = doc.body
+    , methods = ['listen', 'unlisten', 'support']
+    , aliases = ['addEvent', 'removeEvent', 'hasEvent'];
+   
+  aok('methods', !aok.fail(methods, function(n) {
     return typeof avant[n] == 'function';
+  }));
+  
+  aok('aliases', !aok.fail(methods, function(n, i) {
+    return avant[n] === avant[aliases[i]];
   }));
   
   if (!doc) return void aok.log('Basics seem fine. Open test/index.html to run DOM tests.');
 
-  aok('hasEvent-1', true === avant.hasEvent('blur'));
-  aok('hasEvent-2', true === avant.hasEvent('blur', html));
-  aok('hasEvent-3', false === avant.hasEvent('!!!'));
-  aok('hasEvent-4', 2 === aok.pass(['blur', 'click'], avant.hasEvent, body));
+  aok('.support(type)', true === avant.support('blur'));
+  aok('.support(type, elem)', true === avant.support('blur', html));
+  aok('.support(badtype)', false === avant.support('!!!'));
+  aok('.support(type, i)', 2 === aok.pass(['blur', 'click'], avant.support, body));
 }(this));
